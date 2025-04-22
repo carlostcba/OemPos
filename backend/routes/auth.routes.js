d6c1ea1a56354d376a319f484de0350840be1968
carlostcba <33902391+carlostcba@users.noreply.github.com>
2025-04-22 00:54:46 -0300

    Delete backend directory

--- backend/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../controllers/auth.controller');
-
-router.post('/login', auth.login);
-router.post('/register', auth.register); // solo temporal para pruebas
-
-module.exports = router;
